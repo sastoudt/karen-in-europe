70368ef0104faaa9464a936928018754e59f549b
NicoleLincoln <anl004@bucknell.edu>
2026-09-21 14:47:35 -0400

    Still trying

--- Alex.R
+++ Alex.R
@@ -9,6 +9,38 @@ library(dplyr)
 
 ## name_data: subset of names dataset for one name and one country (assumes only females)
 ## karen_data: subset of names dataset of karen from one country (assumes only females) 
-helperCor <- function(full-names-data-nice, karen_data) {
+helperCor <- function(name_data, karen_data) {
   cor(name_data$prop, karen_data$prop, method = "kendall", use = "complete.obs")
 }
+
+name_data<-full_names_data_nice %>%
+  filter(country== "United States") %>%
+  filter(name== "Karen")%>%
+  filter(sex== "F") %>%
+  filter(year >= 1997) %>% 
+  filter( year <= 2014)
+  
+
+SC<- full_names_data_nice %>%
+  filter(country== "Scotland") %>%
+  filter(name== "Karen")%>%
+  filter(sex== "Girl") %>%
+  filter(year >= 1997) %>% 
+  filter( year <= 2014)
+
+NI<-full_names_data_nice %>%
+  filter(country== "Nothern Ireland")%>%
+  filter(name== "Karen")%>%
+  filter(sex== "Girl") %>%
+  filter(year >= 1997) %>% 
+  filter( year <= 2014)
+
+E<-full_names_data_nice %>%
+  filter(country== "England/Wales")%>%
+  filter(name== "Karen")%>%
+  filter(sex== "Girl") %>%
+  filter(year >= 1997) %>% 
+  filter( year <= 2014)
+
+helperCor(name_data, E)
+
